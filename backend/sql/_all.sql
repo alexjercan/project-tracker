091ca58df7a6faa5dd9d0f6429044f21b41b7de9
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-10-30 12:00:46 +0200

    Changed Tables To Use Number Keys

--- backend/sql/_all.sql
+++ backend/sql/_all.sql
@@ -1,5 +1,6 @@
 ﻿create table users
 (
+    user_id    number(10),
     username   varchar2(60),
     password   varchar2(1024),
     first_name varchar2(60),
@@ -7,46 +8,89 @@
     email      varchar2(60)
 );
 
-create unique index users_username_pk on users (username);
+create unique index users_user_id_pk on users (user_id);
 
 alter table users
     add (
-        constraint users_username_pk primary key (username)
+        constraint users_user_id_pk primary key (user_id)
         );
 
 create table projects
 (
-    owner_username varchar2(60),
-    project_name   varchar2(60),
-    description    varchar2(1024),
-    started        date,
-    deadline       date,
-    last_modified  date
+    project_id    number(10),
+    owner_id      number(10),
+    project_name  varchar2(60),
+    description   varchar2(1024),
+    started       date,
+    deadline      date,
+    last_modified date
 );
 
-create unique index projects_project_id_pk on projects (owner_username, project_name);
+create unique index projects_project_id_pk on projects (project_id);
 
 alter table projects
     add (
-        constraint projects_project_id_pk primary key (owner_username, project_name),
-        constraint projects_owner_id_fk foreign key (owner_username) references users (username)
+        constraint projects_project_id_pk primary key (project_id),
+        constraint projects_owner_id_fk foreign key (owner_id) references users (user_id)
         );
 
 create table contributors
 (
-    username       varchar2(60),
-    owner_username varchar2(60),
-    project_name   varchar2(60)
+    contributor_id number(10),
+    user_id        number(10),
+    project_id     number(10)
 );
 
-create unique index contributors_id_pk on contributors (username, owner_username, project_name);
+create unique index contributors_contributor_id_pk on contributors (contributor_id);
 
 alter table contributors
     add (
-        constraint contributors_contributor_id_pk primary key (username, owner_username, project_name),
-        constraint contributors_username_fk foreign key (username) references users (username),
-        constraint contributors_project_id_fk foreign key (owner_username, project_name) references projects (owner_username, project_name)
+        constraint contributors_contributor_id_pk primary key (contributor_id),
+        constraint contributors_user_id_fk foreign key (user_id) references users (user_id),
+        constraint contributors_project_id_fk foreign key (project_id) references projects (project_id)
         );
+
+------ AUTOMATED PK -------
+
+create sequence users_sequence;
+
+create or replace trigger users_on_insert
+    before insert
+    on users
+    for each row
+begin
+    select users_sequence.nextval
+    into :NEW.user_id
+    from dual;
+end;
+/
+
+create sequence projects_sequence;
+
+create or replace trigger projects_on_insert
+    before insert
+    on projects
+    for each row
+begin
+    select projects_sequence.nextval
+    into :NEW.project_id
+    from dual;
+end;
+/
+
+create sequence contributors_sequence;
+
+create or replace trigger contributors_on_insert
+    before insert
+    on contributors
+    for each row
+begin
+    select contributors_sequence.nextval
+    into :NEW.contributor_id
+    from dual;
+end;
+/
+
 
 create or replace procedure insertUser(p_username in string, p_password in string, p_error out number)
     is
@@ -62,6 +106,8 @@ begin
     end;
 end;
 /
+
+------- PROCEDURES --------
 
 create or replace procedure editProfile(p_username in string, p_first_name in string, p_last_name in string,
                                         p_email in string, p_error out number)
@@ -112,19 +158,24 @@ end;
 
 create or replace procedure insertProject(p_project_name in string, p_owner_username in string, p_error out number)
     is
+    v_owner_id   users.user_id%type;
+    v_project_id projects.project_id%type;
 begin
-    begin
-        insert into projects(project_name, owner_username, started, last_modified)
-        values (p_project_name, p_owner_username, sysdate, sysdate);
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
 
-        insert into contributors(username, owner_username, project_name)
-        values (p_owner_username, p_owner_username, p_project_name);
+    insert into projects(project_name, owner_id, started, last_modified)
+    values (p_project_name, v_owner_id, sysdate, sysdate);
+    
+    v_project_id := projects_sequence.currval;
 
-        p_error := 0;
-    exception
-        when others then
-            p_error := 1;
-    end;
+    insert into contributors(user_id, project_id)
+    values (v_owner_id, v_project_id);
+
+    p_error := 0;
+
 end;
 /
 
@@ -132,27 +183,42 @@ create or replace procedure editRepository(p_username in string, p_owner_usernam
                                            p_description in string, p_deadline in string, p_started out string,
                                            p_last_modified out string, p_error out number)
     is
+    v_user_id    users.user_id%type;
+    v_owner_id   users.user_id%type;
+    v_project_id projects.project_id%type;
 begin
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
+
+    select user_id
+    into v_user_id
+    from users
+    where p_username = username;
+
+    select project_id
+    into v_project_id
+    from projects
+    where p_project_name = project_name
+      and v_owner_id = owner_id;
+
     update projects
     set description=p_description,
         deadline=TO_DATE(p_deadline, 'DD-MM-YYYY HH:MI A.M.'),
         last_modified=sysdate
-    where p_owner_username = owner_username
-      and p_project_name = project_name
-      and p_username in (select username
-                         from contributors c
-                         where p_owner_username = c.owner_username
-                           and p_project_name = c.project_name);
+    where v_project_id = project_id
+      and v_user_id in (select user_id
+                        from contributors c
+                        where project_id = v_project_id);
 
     select TO_CHAR(started, 'DD-MM-YYYY HH:MI A.M.'), TO_CHAR(last_modified, 'DD-MM-YYYY HH:MI A.M.')
     into p_started, p_last_modified
     from projects
-    where p_owner_username = owner_username
-      and p_project_name = project_name
-      and p_username in (select username
-                         from contributors c
-                         where p_owner_username = c.owner_username
-                           and p_project_name = c.project_name);
+    where v_project_id = project_id
+      and v_user_id in (select user_id
+                        from contributors c
+                        where project_id = v_project_id);
 
     p_error := 0;
 exception
@@ -164,13 +230,19 @@ end;
 create or replace procedure doesProjectExists(p_project_name in string, p_owner_username in string, p_error out number)
     is
     result_count number := 0;
+    v_owner_id   users.user_id%type;
 begin
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
+
     select count(*)
     into result_count
     from projects
     where p_project_name = project_name
-      and p_owner_username = owner_username
-    group by project_name, owner_username;
+      and v_owner_id = owner_id
+    group by project_name, owner_id;
 
     if result_count = 0 then
         p_error := 0;
@@ -185,13 +257,19 @@ end;
 
 create or replace procedure getProjects(p_username in string, p_cursor out sys_refcursor, p_error out number)
     is
+    v_user_id users.user_id%type;
 begin
+    select user_id
+    into v_user_id
+    from users
+    where p_username = username;
+
     open p_cursor for
-        select project_name, owner_username
+        select p.project_name, o.username
         from projects p
-                 natural join contributors c
-                 natural join users u
-        where p_username = username;
+                 join users o on p.owner_id = o.user_id
+                 join contributors c on p.project_id = c.project_id
+        where c.user_id = v_user_id;
 
     p_error := 0;
 exception
@@ -204,17 +282,36 @@ create or replace procedure getRepository(p_username in string, p_owner_username
                                           p_description out string, p_started out string, p_deadline out string,
                                           p_last_modified out string, p_error out number)
     is
+    v_user_id    users.user_id%type;
+    v_owner_id   users.user_id%type;
+    v_project_id projects.project_id%type;
 begin
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
+
+    select user_id
+    into v_user_id
+    from users
+    where p_username = username;
+
+    select project_id
+    into v_project_id
+    from projects
+    where p_project_name = project_name
+      and v_owner_id = owner_id;
+
     select description,
            TO_CHAR(started, 'DD-MM-YYYY HH:MI A.M.'),
            TO_CHAR(deadline, 'DD-MM-YYYY HH:MI A.M.'),
            TO_CHAR(last_modified, 'DD-MM-YYYY HH:MI A.M.')
     into p_description, p_started, p_deadline, p_last_modified
     from projects
-             natural join contributors
-    where p_owner_username = owner_username
-      and p_project_name = project_name
-      and p_username = username;
+    where project_id = v_project_id
+      and v_user_id in (select user_id
+                        from contributors c
+                        where project_id = v_project_id);
 
     p_error := 0;
 exception
@@ -226,16 +323,33 @@ end;
 create or replace procedure insertContributor(p_project_name in string, p_owner_username in string,
                                               p_contributor_username in string, p_error out number)
     is
+    v_user_id    users.user_id%type;
+    v_project_id projects.project_id%type;
+    v_owner_id   users.user_id%type;
 begin
-    begin
-        insert into contributors(username, owner_username, project_name)
-        values (p_contributor_username, p_owner_username, p_project_name);
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
 
-        p_error := 0;
-    exception
-        when others then
-            p_error := 1;
-    end;
+    select user_id
+    into v_user_id
+    from users
+    where p_contributor_username = username;
+
+    select project_id
+    into v_project_id
+    from projects
+    where p_project_name = project_name
+      and v_owner_id = owner_id;
+
+    insert into contributors(user_id, project_id)
+    values (v_user_id, v_project_id);
+
+    p_error := 0;
+exception
+    when others then
+        p_error := 1;
 end;
 /
 
@@ -243,14 +357,32 @@ create or replace procedure doesContributorExists(p_project_name in string, p_ow
                                                   p_contributor_username in string, p_error out number)
     is
     result_count number := 0;
+    v_user_id    users.user_id%type;
+    v_project_id projects.project_id%type;
+    v_owner_id   users.user_id%type;
 begin
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
+
+    select user_id
+    into v_user_id
+    from users
+    where p_contributor_username = username;
+
+    select project_id
+    into v_project_id
+    from projects
+    where p_project_name = project_name
+      and v_owner_id = owner_id;
+
     select count(*)
     into result_count
     from contributors
-    where p_project_name = project_name
-      and p_owner_username = owner_username
-      and p_contributor_username = username
-    group by project_name, owner_username, username;
+    where v_project_id = project_id
+      and v_user_id = user_id
+    group by project_id, user_id;
 
     if result_count = 0 then
         p_error := 0;
@@ -266,12 +398,27 @@ end;
 create or replace procedure getContributors(p_owner_username in string, p_project_name in string,
                                             p_cursor out sys_refcursor, p_error out number)
     is
+    v_project_id projects.project_id%type;
+    v_owner_id   users.user_id%type;
 begin
+    select user_id
+    into v_owner_id
+    from users
+    where p_owner_username = username;
+
+    select project_id
+    into v_project_id
+    from projects
+    where p_project_name = project_name
+      and v_owner_id = owner_id;
+
+
     open p_cursor for
         select project_name, username
-        from contributors p
-        where p_owner_username = owner_username
-          and p_project_name = project_name;
+        from contributors
+            natural join users
+            natural join projects
+        where v_project_id = project_id;
 
     p_error := 0;
 exception
